c63239c3f8f4d0530c0f532abe2c2e2325ec92d9
Mark Mestern <mark.mestern@uswitch.com>
2017-05-19 17:23:57 +0100

    Uses configured namespace instead of default

--- scale/scale.go
+++ scale/scale.go
@@ -68,9 +68,9 @@ func (p *PodAutoScaler) ScaleUp() error {
 
 func (p *PodAutoScaler) ScaleDown() error {
 	log.Infof("Scaledown call, deployment api call : p.Client.Deployments(" + p.Namespace + ").Get(" + p.Deployment + ") ")
-	deployment, err := p.Client.Deployments(api.NamespaceDefault).Get(p.Deployment)
+	deployment, err := p.Client.Deployments(p.Namespace).Get(p.Deployment)
 	if err != nil {
-		return errors.Wrap(err, "Failed to get deployment from kube server, no scale up occured")
+		return errors.Wrap(err, "Failed to get deployment from kube server, no scale down occured")
 	}
 
 	currentReplicas := deployment.Spec.Replicas
