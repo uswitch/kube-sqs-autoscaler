02fcc0e8873cb72ba5569b2a2793541b900bc1f9
Mark Mestern <mark.mestern@uswitch.com>
2017-05-22 17:44:44 +0100

    reads json config file - durations not working

--- main.go
+++ main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
+	io "io/ioutil"
 	"time"
 
 	log "github.com/Sirupsen/logrus"
+	"os"
 
 	"github.com/uswitch/kube-sqs-autoscaler/scale"
 	"github.com/uswitch/kube-sqs-autoscaler/sqs"
@@ -23,6 +26,7 @@ var (
 	sqsQueueUrl              string
 	kubernetesDeploymentName string
 	kubernetesNamespace      string
+	configFile               string
 )
 
 func Run(p *scale.PodAutoScaler, sqs *sqs.SqsClient) {
@@ -62,7 +66,7 @@ func Run(p *scale.PodAutoScaler, sqs *sqs.SqsClient) {
 						log.WithFields(log.Fields{"kubernetesDeploymentName": kubernetesDeploymentName}).Info("Waiting for cool off, skipping scale down")
 						continue
 					}
-					log.WithFields(log.Fields{"kubernetesDeploymentName": kubernetesDeploymentName, "scaleUpMessages": scaleUpMessages, "numMessages": numMessages}).Info("Scale down may be appropriate")
+					log.WithFields(log.Fields{"kubernetesDeploymentName": kubernetesDeploymentName, "scaleDownMessages": scaleDownMessages, "numMessages": numMessages}).Info("Scale down may be appropriate")
 					if changed, err = p.Scale(scale.DOWN); err != nil {
 						log.WithFields(log.Fields{"kubernetesDeploymentName": kubernetesDeploymentName}).Errorf("Failed scaling down: %v", err)
 						continue
@@ -75,6 +79,46 @@ func Run(p *scale.PodAutoScaler, sqs *sqs.SqsClient) {
 		}
 	}
 
+}
+
+type Configuration struct {
+	PollInterval             time.Duration `json:"pollperiod"`
+	ScaleDownCoolPeriod      time.Duration `json:"scaledowncooloff"`
+	ScaleUpCoolPeriod        time.Duration `json:"scaleupcooloff"`
+	ScaleDownMessages        int           `json:"scaleDownMessages"`
+	ScaleUpMessages          int           `json:"scaleupmessages"`
+	MaxPods                  int           `json:"maxpods"`
+	MinPods                  int           `json:"minpods`
+	AwsRegion                string        `json:"awsregion"`
+	SqsQueueUrl              string        `json:"sqs-queue-url"`
+	KubernetesDeploymentName string        `json:"kubernetesdeployment"`
+	KubernetesNamespace      string        `json:"kubernetesnamespace"`
+	Active                   bool          `json:"active"`
+}
+
+type ConfigurationOld struct {
+	Id      int
+	Key     int `json:"keyhere"`
+	MaxPods int `json:"maxpods"`
+}
+
+// Returns array of configurations to use.
+//TODO fix duration parsing
+func parseConfigFile(configFileName string) (configList []Configuration, err error) {
+	//configList = make([]Configuration, 0)
+	data, err := io.ReadFile(configFileName)
+	log.Info("data read as %v", string(data))
+	if err != nil {
+		log.Errorf("Unable to open file %v", configFileName)
+		return nil, err
+	}
+	err = json.Unmarshal(data, &configList)
+	for i := 0; i < len(configList); i += 1 {
+		v := configList[i]
+		log.Infof("conf %v read as %+v", i, v)
+	}
+
+	return configList, nil
 }
 
 func main() {
@@ -91,8 +135,18 @@ func main() {
 	flag.StringVar(&kubernetesDeploymentName, "kubernetes-deployment", "", "Kubernetes Deployment to scale. This field is required")
 	flag.StringVar(&kubernetesNamespace, "kubernetes-namespace", "default", "The namespace your deployment is running in")
 
+	flag.StringVar(&configFile, "config-file", "", "Configuration by json file, array of maps of the above attributes")
 	flag.Parse()
 
+	if configFile != "" {
+		configList, err := parseConfigFile(configFile)
+		if err != nil {
+			log.Errorf("Unable to parse file %v", configFile)
+			return
+		}
+		log.Infof("config read, got %v items", len(configList))
+		return
+	}
 	log.Info("Starting kube-sqs-autoscaler for deployment " + kubernetesDeploymentName + " and namespace " + kubernetesNamespace)
 	p := scale.NewPodAutoScaler(kubernetesDeploymentName, kubernetesNamespace, maxPods, minPods)
 	sqs := sqs.NewSqsClient(sqsQueueUrl, awsRegion)
